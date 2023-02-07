324f58a12862c048b240bbe9e8c015b2cceae3b8
Aniruddha Sil <welcometohellv@gmail.com>
2023-02-08 01:39:41 +0530

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   semi: false,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   singleQuote: true,
   tabWidth: 2,
   useTabs: false,
-};
+  endOfLine: 'lf',
+  printWidth: 120,
+}
