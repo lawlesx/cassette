d780fea8530bd8755fd54afc48c346046ce1d704
Aniruddha Sil <welcometohellv@gmail.com>
2023-02-08 01:16:39 +0530

    remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <main className="bg-black min-h-screen grid place-items-center">
