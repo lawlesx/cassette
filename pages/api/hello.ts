38a050b55ea5d06db438a2c7a97f604b76317866
Aniruddha Sil <welcometohellv@gmail.com>
2023-02-08 01:26:01 +0530

    Installed supabase client

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { supabase } from '@/lib/supabaseClient'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  name: any
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+
+  // Change this to get data from your table 
+  const { data } = await supabase.from('countries').select()
+
+
+  res.status(200).json({ name: data })
 }
