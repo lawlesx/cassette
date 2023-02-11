cd53ba583745f586c805e431e13af8f53d03945e
Aniruddha Sil <welcometohellv@gmail.com>
2023-02-11 23:59:49 +0530

    Update Test.tsx

--- components/Test.tsx
+++ components/Test.tsx
@@ -3,15 +3,13 @@ import axios from "axios";
 import { useEffect } from "react";
 
 const Test = () => {
-
-  const body = {
-    wallet_address: '0x123',
-    name: 'test',
-    user_name: 'test_username',
-    profile_image: 'test_profile_image',
-  }
-
   useEffect(() => {
+    const body = {
+      wallet_address: '0x123',
+      name: 'test',
+      user_name: 'test_username',
+      profile_image: 'test_profile_image',
+    }
     axios.post('http://localhost:3000/api/createUser', body).then((res) => {
       console.log(res.data);
     })
