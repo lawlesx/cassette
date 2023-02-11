e142bd30c61ddf2642632989cf70a576499bb238
Aniruddha Sil <welcometohellv@gmail.com>
2023-02-11 12:57:36 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { Stream } from "@/components/Stream";
-
 export default function Home() {
   return (
     <main className="min-h-screen grid place-items-center">
       <h1 className='text-[5rem] text-red-400'>Cassette</h1>
-      <Stream />
     </main>
   )
 }
