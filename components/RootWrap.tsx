165558cff9155f4db2456d9a4aaf5111e5caed35
Aniruddha Sil <welcometohellv@gmail.com>
2023-02-12 16:36:19 +0530

    Installed wagmi ethers and axiso

--- components/RootWrap.tsx
+++ components/RootWrap.tsx
@@ -1,16 +1,34 @@
 'use client'
 import { FC, ReactNode } from 'react'
 import { createReactClient, LivepeerConfig, studioProvider } from '@livepeer/react'
+import { WagmiConfig, createClient, configureChains } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+import { polygonMumbai, polygon } from 'wagmi/chains'
 
-const client = createReactClient({
+const { chains, provider, webSocketProvider } = configureChains(
+  [polygon, polygonMumbai],
+  [publicProvider()],
+)
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+})
+
+const livePeerClient = createReactClient({
   provider: studioProvider({ apiKey: process.env.NEXT_PUBLIC_LIVEPEER_STUDIO_API_KEY as string }),
 })
 
 const RootWrap: FC<{ children: ReactNode }> = ({ children }) => {
+  console.log('Supported Chains', chains);
+
   return (
-    <LivepeerConfig client={client}>
-      {children}
-    </LivepeerConfig>
+    <WagmiConfig client={wagmiClient}>
+      <LivepeerConfig client={livePeerClient}>
+        {children}
+      </LivepeerConfig>
+    </WagmiConfig>
   )
 }
 
