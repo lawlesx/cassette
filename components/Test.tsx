ab505998047d482725ca49cb9bff397d3de8cee5
Atul1223 <a1atulk@gmail.com>
2023-02-13 20:36:42 +0530

    create|fetch stream data and buy nft backend
    Signed-off-by: Atul1223 <a1atulk@gmail.com>

--- components/Test.tsx
+++ components/Test.tsx
@@ -27,26 +27,49 @@ const Test = () => {
 
 
   useEffect(() => {
+    // const body = {
+    //   nft_address : 'testAddress1',
+    //   mint_price : 1.2,
+    //   nft_quantity: 30,
+    //   nft_title: "New NFT",
+    //   nft_base_uri: "base Uri",
+    //   nft_image_uri: "imageUri",
+    //   nft_symbol: "nft symbol",
+    //   nft_owner_address: "0x123",
+    //   nft_sold_count: 0,
+    //   nft_count: 1
+    // }
     const body = {
-      nft_address : 'testAddress1',
-      mint_price : 1.2,
-      nft_quantity: 30,
-      nft_title: "New NFT",
-      nft_base_uri: "base Uri",
-      nft_image_uri: "imageUri",
-      nft_symbol: "nft symbol",
-      nft_owner_address: "0x123",
-      nft_sold_count: 0,
-      nft_count: 1
+      wallet_address : '0x123',
+      bought_nft_address : 'testAddress1',
+      token_id : 1
     }
     try{
-      axios.post('http://localhost:3000/api/updateNft', body).then((res) => {
+      axios.post('http://localhost:3000/api/buyNft', body).then((res) => {
           console.log(res.data);
         })
     }catch(error){
       console.log(error)
     }
   }, []);
+
+  // useEffect(() => {
+  //   const body = {
+  //     stream_key : 'teststream1',
+  //     stream_name : 'hello',
+  //     stream_url: 'http:test.test',
+  //     streamer_wallet_address: "0x123",
+  //     streamer_user_name: "testUserName",
+  //     nft_address: "testAddress1",
+  //   }
+  //   try{
+  //     axios.post('http://localhost:3000/api/fetchStream', body).then((res) => {
+  //         console.log(res.data);
+  //       })
+  //   }catch(error){
+  //     console.log(error)
+  //   }
+  // }, []);
 
   return <div>Test</div>;
 };
---
+++ pages/api/fetchStream.ts
@@ -0,0 +1,18 @@
+import { supabase } from "@/lib/supabaseClient";
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: any
+  error: any
+}
+
+//fetch user data based on stream key
+//pass stream key in body
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    try{
+        const {data, error} = await supabase.from('tbl_stream').select().eq('stream_key', req.body.stream_key)
+        res.status(200).json({ name: data, error: false })
+    }catch(error){
+        res.status(200).json({ name: 'streamDataNotFound', error: true })
+    }
+}
