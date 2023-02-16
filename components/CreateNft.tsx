f021bff7dca26267d6ab4493e8bf66b97a412cba
Aniruddha Sil <welcometohellv@gmail.com>
2023-02-16 22:43:56 +0530

    Partial

--- components/CreateNft.tsx
+++ components/CreateNft.tsx
@@ -1,29 +1,11 @@
 'use client'
-import { CassetteUnlockFactory, CassetteUnlockFactory__factory } from '@/contracts/abis/types';
+import { CassetteUnlockFactory__factory } from '@/contracts/abis/types';
 import contractAddress from '@/utils/contract';
 import { PublicLockV12 } from '@unlock-protocol/contracts'
-import { BigNumber, ethers } from 'ethers';
-import { useContract, useContractWrite, usePrepareContractWrite, useProvider, useSigner } from 'wagmi'
+import { ethers } from 'ethers';
+import { useContractWrite, usePrepareContractWrite } from 'wagmi'
 
 const CreateNft = () => {
-  // const { data: signer, isError, isLoading } = useSigner()
-
-  // const onCreate = async () => {
-
-
-  //   if (!signer) {
-  //     console.log('No signer', signer);
-  //     return
-  //   }
-
-  //   const unlock = new ethers.Contract(contractAddress.cassetteUnlockFactory[80001], CassetteUnlockFactory__factory.abi, signer);
-  //   const transaction = await unlock.deployLock("0x3D02B87ae906F1D6f130832f67E5c10C9f869205", params, 'New Unlock Membership Testing', 'UNLOCKTESTING', 'ipfs://bafybeifrnutuq4zdrzg7zyxwxk2qhvmybyvz43tg3h7bequ5ywd6ippgsi/')
-  //   console.log('transaction', transaction);
-  //   const receipt = await transaction.wait();
-  //   const lockAddress = receipt.logs[0].address;
-  //   console.log(lockAddress);
-  // }
-
   const lockInterface = new ethers.utils.Interface(PublicLockV12.abi);
   const params = lockInterface.encodeFunctionData(
     "initialize(address,uint256,address,uint256,uint256,string)",
@@ -49,7 +31,7 @@ const CreateNft = () => {
     address: contractAddress.cassetteUnlockFactory[80001],
     abi: CassetteUnlockFactory__factory.abi,
     functionName: 'deployLock',
-    args: ["0x3D02B87ae906F1D6f130832f67E5c10C9f869205", params as `0x${string}`, 'New Unlock Membership Testing', 'UNLOCKTESTING', 'ipfs://bafybeifrnutuq4zdrzg7zyxwxk2qhvmybyvz43tg3h7bequ5ywd6ippgsi/'],
+    args: ["0x3D02B87ae906F1D6f130832f67E5c10C9f869205", params as `0x${string}`, 'LOL Unlock Membership Testing', 'LOLUNLOCKTESTING', 'ipfs://bafybeifrnutuq4zdrzg7zyxwxk2qhvmybyvz43tg3h7bequ5ywd6ippgsi/'],
   })
   const { data, isLoading, isSuccess, write } = useContractWrite(config)
   console.log('Data', data, write)
--- utils/contract.ts
+++ utils/contract.ts
@@ -1,6 +1,6 @@
 const contractAddress: Record<string, Record<number, `0x${string}`>> = {
   cassetteUnlockFactory: {
-    80001: '0x6fB2b3091413Dbd5ce167F021584557a24D3C195',
+    80001: '0xcdFA2081A18196A9A0832E2B7E9E202FF2552a00',
   },
 }
 
