b7677cd149ba1d733d188a6cba6cc517c79c1114
Aniruddha Sil <welcometohellv@gmail.com>
2023-02-19 00:06:59 +0530

    Save nft in backend

--- components/NFTCreation/index.tsx
+++ components/NFTCreation/index.tsx
@@ -9,12 +9,13 @@ import { parseUnits } from 'ethers/lib/utils.js'
 import { ethers } from 'ethers'
 import { PublicLockV12 } from '@unlock-protocol/contracts'
 import contractAddress from '@/utils/contract'
-import { useContractWrite, usePrepareContractWrite } from 'wagmi'
+import { useAccount, useContractEvent, useContractWrite, usePrepareContractWrite } from 'wagmi'
 import { CassetteUnlockFactory__factory } from '@/contracts/abis/types'
-import { useCallback, useRef } from 'react'
+import { useCallback, useRef, useState } from 'react'
 import storeNft from '@/lib/nftStorage'
 import { useMutation } from 'react-query'
 import { SECONDS_PER_DAY } from '@/helpers/constants'
+import axios from 'axios'
 
 const schema = yup
   .object({
@@ -38,8 +39,12 @@ const NftCreation = () => {
     },
   })
 
+  const { address: userAddress } = useAccount()
+
+  const [isCreatingNft, setisCreatingNft] = useState(false)
   const param = useRef<`0x${string}`>(ethers.constants.AddressZero)
   const nftTokenUri = useRef<string>('')
+  const nftImageUri = useRef<string>('')
 
   const {
     watch,
@@ -51,7 +56,7 @@ const NftCreation = () => {
     abi: CassetteUnlockFactory__factory.abi,
     functionName: 'deployLock',
     args: [
-      '0x3D02B87ae906F1D6f130832f67E5c10C9f869205',
+      userAddress as `0x${string}`,
       param.current,
       watch('name'),
       watch('symbol'),
@@ -60,6 +65,32 @@ const NftCreation = () => {
   })
 
   const { data, isSuccess, write } = useContractWrite(config)
+
+  useContractEvent({
+    address: contractAddress.cassetteUnlockFactory[80001],
+    abi: CassetteUnlockFactory__factory.abi,
+    eventName: 'LockDeployed',
+    listener: async (lock, deployer, owner) => {
+      console.log(lock, deployer, owner)
+      const data = watch()
+      const body = {
+        nft_address: lock,
+        mint_price: data.price,
+        nft_quantity: data.quantity,
+        nft_title: data.name,
+        nft_image_uri: nftImageUri.current,
+        nft_symbol: data.symbol,
+        nft_owner_address: userAddress,
+        nft_sold_count: 0,
+      }
+      const res = await axios.post('/api/createNft', body)
+      if (!res.data.error) {
+        setisCreatingNft(false)
+      }
+      console.log(res.data);
+
+    },
+  })
 
   const onPrepareMetadata = useCallback(async () => {
     const data = watch()
@@ -82,7 +113,7 @@ const NftCreation = () => {
     ])
     param.current = _params as `0x${string}`
 
-    const { nftTokenUri: _nftTokenUri } = await storeNft({
+    const { nftTokenUri: _nftTokenUri, imageUri: _imageUri } = await storeNft({
       name: data.name,
       feeRecipient: '0x3D02B87ae906F1D6f130832f67E5c10C9f869205',
       count: data.quantity,
@@ -92,12 +123,14 @@ const NftCreation = () => {
       description: `Watch ${data.name} Exclusively on Cassette`,
     })
     nftTokenUri.current = `ipfs://${_nftTokenUri}/`
+    nftImageUri.current = `ipfs://${_imageUri}`
   }, [watch])
 
   const { mutate: mutateMetadata, isLoading } = useMutation('upload-metadata', onPrepareMetadata)
 
   const onSubmit = (data: FormData) => {
     console.log('lol', data)
+    setisCreatingNft(true)
     write?.()
   }
 
@@ -112,7 +145,7 @@ const NftCreation = () => {
           {!write && <Button disabled={!isValid || isLoading} onClick={() => mutateMetadata()}>
             {isLoading ? 'Uploading' : 'Prepare NFT metadata'}
           </Button>}
-          <Button disabled={!write} type="submit">{isSuccess ? 'Created' : 'Create NFT'}</Button>
+          <Button disabled={!write} type="submit">{isSuccess ? isCreatingNft ? 'Creating' : 'Created' : 'Create NFT'}</Button>
         </div>
       </form>
     </FormProvider>
