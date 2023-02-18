7772470b0466a1781364cc403698d90e45a738b5
Aniruddha Sil <welcometohellv@gmail.com>
2023-02-18 21:59:58 +0530

    Added Stream UI

--- app/stream/page.tsx
+++ app/stream/page.tsx
@@ -1,9 +1,11 @@
-import { Stream } from "@/components/Stream";
+import { Stream } from '@/components/Stream'
 
 const Page = () => {
-  return <div className="w-full h-full flex items-center justify-center p-20">
-    <Stream />
-  </div>
-};
+  return (
+    <div className="w-full h-full flex items-center justify-center p-20">
+      <Stream />
+    </div>
+  )
+}
 
-export default Page;
+export default Page
