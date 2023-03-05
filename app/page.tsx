217c9304bb5e06f2e3669b109d5f4c5d079507c1
Aniruddha Sil <welcometohellv@gmail.com>
2023-03-05 12:28:03 +0530

    Landing preview image

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
-import Link from "next/link";
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="w-full p-10">
-      <Link href="/stream" className="bg-red-400 rounded-xl p-4 text-lg text-white font-medium">Create Stream</Link>
+      <div className="flex flex-col items-center px-10">
+        <div className="w-full h-[21rem] relative rounded-lg overflow-hidden flex flex-col-reverse">
+          <div className="absolute h-full w-full">
+            <Image src="/images/landing.jpg" alt="Landing" fill style={{ objectFit: 'cover' }} priority />
+          </div>
+          {/* ------------------------------ Overlay Mask ------------------------------ */}
+          <svg className='w-full h-full absolute -bottom-1 left-0' viewBox="0 0 1312 467" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1311.73 0.203438H0.273193V466.796H1311.73V0.203438Z" fill="url(#paint0_linear_1_3)" />
+            <defs>
+              <linearGradient
+                id="paint0_linear_1_3"
+                x1="656"
+                y1="0.203438"
+                x2="656"
+                y2="466.796"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopOpacity="0" />
+                <stop offset="0.583333" stopColor="#030303" stopOpacity="0" />
+                <stop offset="1" stopColor="#030303" stopOpacity="0.94" />
+              </linearGradient>
+            </defs>
+          </svg>
+
+          <p className="text-5xl text-primary font-bold relative w-1/2 px-8 py-6">
+            Catch your favourite creators on
+            <span className="text-vibrant pl-2">Cassette</span>
+          </p>
+        </div>
+      </div>
     </main>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,20 @@
 'use client'
 
-import Link from "next/link"
-import ConnectWallet from "./Buttons/ConnectWallet"
+import Link from 'next/link'
+import ConnectWallet from './Buttons/ConnectWallet'
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between bg-white bg-opacity-[0.07] backdrop-blur-xl px-20 py-6 rounded-2xl">
-      <Link href="/" className="text-4xl font-bold text-primary">Cassette</Link>
-      <ConnectWallet />
+    <nav className="flex items-center justify-between bg-white bg-opacity-[0.07] backdrop-blur-xl px-20 py-4 rounded-b-2xl">
+      <Link href="/" className="text-4xl font-bold text-primary">
+        Cassette
+      </Link>
+      <div className='flex items-center gap-8'>
+        <Link href="/stream" className="bg-red-400 rounded-xl p-4 text-lg text-white font-medium">
+          Create Stream
+        </Link>
+        <ConnectWallet />
+      </div>
     </nav>
   )
 }
